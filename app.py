0b81f94307d931d5c772bb624ff886c189626f10
RICHIES876 <richmars876@gmail.com>
2025-01-31 14:07:59 -0500

    created a temperature check functin

--- app.py
+++ app.py
@@ -20,6 +20,17 @@ def potential_divider(supply_voltage, resistors_value):
 
 potential_divider(5, [4, 5])
 
+def Temperature_check(temp_val, unit_sign):
+    if unit_sign == "C":
+        degree = (temp_val -32)* (5/9)
+    elif unit_sign == "F":
+        degree = temp_val*(5/9)+ 32
+    if temp_val < 29:
+         print("The Patient is Hypothermic")
+    else: 
+        print("The Patient is Hyperthermic")
+
+Temperature_check(35, "C")
 
 
     
